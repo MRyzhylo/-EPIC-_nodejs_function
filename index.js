a9b3f5330dddf1892161f141316b7b954e5de399
MRyzhylo <playa77777@gmail.com>
2019-09-03 17:27:11 +0300

    complete task

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const request = require('request');
+
+const request = require("request");
 
 const options = {
     method: 'GET',
     url: 'https://api.trello.com/1/members/mryzhylo/cards',
     qs: {
         json: true,
-        filter: 'visible',
+        filter: 'all',
         key: 'cfcc3b3e399c10cedb3b8ff3053624b2',
         token: '7c7caa6513248502b1b89e9c9fba45d1c9a6bd308b5e074e7e64ad50e3affa14'
     }
 };
 
+function callback(error, response, body) {
+    if (error) { return console.log(error); }
 
-request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-  
-    console.log(body);
-  });
+    const info = JSON.parse(body);
+    
+    function findCard(info) {
+        let taskName = '[EPIC] nodejs function';
+        if (info.name === taskName) {
+            return {}
+        } else {
+            return false
+        }
+    }
+
+    let card = info.find(findCard);
+    console.log(card.desc);
+    
+}
+
+request(options, callback);
